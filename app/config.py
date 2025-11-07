5aa64ddacaae267e30b9eec76655432db32de615
OlaruMariaMadalina <maria-madalina.olaru@endava.com>
2025-11-07 12:53:49 +0200

    restore config

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    app_name: str = "slip-salary-app"               
+    database_url: str = "sqlite+aiosqlite:///./dev.db"
+    JWT_SECRET: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_EXPIRE_MINUTES: int = 60
+    SMTP_SERVER: str = "smtp.gmail.com"
+    SMTP_PORT: int = 587
+    SMTP_USERNAME: str = "mariamadalinaolaru26@gmail.com"
+    SMTP_PASSWORD: str = "xlwfmzaakoeqtnnw"
+    
+    
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
+settings = Settings()
